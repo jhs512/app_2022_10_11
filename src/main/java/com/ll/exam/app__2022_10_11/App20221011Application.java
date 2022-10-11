2aa7e5028161186cb57cc21b3c188a2d194d5ff8
jhs512 <jangka512@gmail.com>
2022-10-11 13:11:30 +0900

    세팅

---
+++ src/main/java/com/ll/exam/app__2022_10_11/App20221011Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app__2022_10_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App20221011Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App20221011Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/exam/app__2022_10_11/App20221011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app__2022_10_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App20221011ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
