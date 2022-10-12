0a3c7674ce5dbb79e7db7028c4209a3ff7dea48f
jhs512 <jangka512@gmail.com>
2022-10-12 16:07:19 +0900

    관리자

---
+++ src/main/java/com/ll/exam/app__2022_10_11/app/rebate/controller/AdmRebateController.java
@@ -0,0 +1,18 @@
+package com.ll.exam.app__2022_10_11.app.rebate.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/adm/rebate")
+public class AdmRebateController {
+    @GetMapping("/makeData")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String showMakeData() {
+        return "adm/rebate/makeData";
+    }
+}
+
+
