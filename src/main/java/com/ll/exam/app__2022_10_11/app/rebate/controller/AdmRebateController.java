321493fc573544a05e98af3cdcba702c0091631d
jhs512 <jangka512@gmail.com>
2022-10-14 14:18:22 +0900

    폼처리

--- src/main/java/com/ll/exam/app__2022_10_11/app/rebate/controller/AdmRebateController.java
+++ src/main/java/com/ll/exam/app__2022_10_11/app/rebate/controller/AdmRebateController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -60,12 +61,32 @@ public class AdmRebateController {
         RsData rebateRsData = rebateService.rebate(orderItemId);
 
         String referer = req.getHeader("Referer");
-        log.debug("referer : " + referer);
         String yearMonth = Ut.url.getQueryParamValue(referer, "yearMonth", "");
 
         String redirect = "redirect:/adm/rebate/rebateOrderItemList?yearMonth=" + yearMonth;
 
         redirect = rebateRsData.addMsgToUrl(redirect);
+
+        return redirect;
+    }
+
+    @PostMapping("/rebate")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String rebate(String ids, HttpServletRequest req) {
+
+        String[] idsArr = ids.split(",");
+
+        Arrays.stream(idsArr)
+                .mapToLong(Long::parseLong)
+                .forEach(id -> {
+                    rebateService.rebate(id);
+                });
+
+        String referer = req.getHeader("Referer");
+        String yearMonth = Ut.url.getQueryParamValue(referer, "yearMonth", "");
+
+        String redirect = "redirect:/adm/rebate/rebateOrderItemList?yearMonth=" + yearMonth;
+        redirect += "&msg=" + Ut.url.encode("%d건의 정산품목을 정산처리하였습니다.".formatted(idsArr.length));
 
         return redirect;
     }
