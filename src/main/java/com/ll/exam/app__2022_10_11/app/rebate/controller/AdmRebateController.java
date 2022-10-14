fde7f80a2b01ad61180c15c29a777089809281d1
jhs512 <jangka512@gmail.com>
2022-10-14 11:03:21 +0900

    22 10 14, ken 10764, 29강, RebateOrderItem 목록 페이지 추가

--- src/main/java/com/ll/exam/app__2022_10_11/app/rebate/controller/AdmRebateController.java
+++ src/main/java/com/ll/exam/app__2022_10_11/app/rebate/controller/AdmRebateController.java
@@ -8,9 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/adm/rebate")
@@ -26,11 +23,20 @@ public class AdmRebateController {
 
     @PostMapping("/makeData")
     @PreAuthorize("hasAuthority('ADMIN')")
-    @ResponseBody
     public String makeData(String yearMonth) {
         rebateService.makeDate(yearMonth);
 
-        return "성공";
+        return "redirect:/adm/rebate/rebateOrderItemList?yearMonth=" + yearMonth + "&msg=" + Ut.url.encode("정산데이터가 성공적으로 생성되었습니다.");
+    }
+
+    @GetMapping("/rebateOrderItemList")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String showRebateOrderItemList(String yearMonth) {
+        if (yearMonth == null) {
+            yearMonth = "2022-10";
+        }
+
+        return "adm/rebate/rebateOrderItemList";
     }
 }
 
