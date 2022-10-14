fb03a7d6af4c9c67a2c662da1e614849bd947dea
jhs512 <jangka512@gmail.com>
2022-10-14 10:43:56 +0900

    RebateOrderItem 엔티티에 구매자 정보, 정산금액지급내역번호 추가

--- src/main/java/com/ll/exam/app__2022_10_11/app/rebate/entity/RebateOrderItem.java
+++ src/main/java/com/ll/exam/app__2022_10_11/app/rebate/entity/RebateOrderItem.java
@@ -2,6 +2,8 @@ package com.ll.exam.app__2022_10_11.app.rebate.entity;
 
 
 import com.ll.exam.app__2022_10_11.app.base.entity.BaseEntity;
+import com.ll.exam.app__2022_10_11.app.cash.entity.CashLog;
+import com.ll.exam.app__2022_10_11.app.member.entity.Member;
 import com.ll.exam.app__2022_10_11.app.order.entity.Order;
 import com.ll.exam.app__2022_10_11.app.order.entity.OrderItem;
 import com.ll.exam.app__2022_10_11.app.product.entity.Product;
@@ -47,11 +49,23 @@ public class RebateOrderItem extends BaseEntity {
     private boolean isPaid; // 결제여부
     private LocalDateTime payDate; // 결제날짜
 
+    @ManyToOne(fetch = LAZY)
+    @ToString.Exclude
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private CashLog rebateCashLog; // 정산에 관련된 환급지급내역
+
     // 상품
     private String productSubject;
 
     // 주문품목
     private LocalDateTime orderItemCreateDate;
+
+    // 회원
+    @ManyToOne(fetch = LAZY)
+    @ToString.Exclude
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Member buyer;
+    private String buyerName;
 
     public RebateOrderItem(OrderItem orderItem) {
         this.orderItem = orderItem;
@@ -71,5 +85,9 @@ public class RebateOrderItem extends BaseEntity {
 
         // 주문품목 추가데이터
         orderItemCreateDate = orderItem.getCreateDate();
+
+        // 주문품목 추가데이터
+        buyer = orderItem.getOrder().getBuyer();
+        buyerName = orderItem.getOrder().getBuyer().getName();
     }
 }
